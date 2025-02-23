ba46337356e7c8f937850a854acc52ef36907fbe
Eric Tong <erictong@college.harvard.edu>
2025-02-23 11:58:15 -0500

    Added events and images to page

--- src/Components/event.js
+++ src/Components/event.js
@@ -1,4 +1,122 @@
 exports.events = [
+    /* {
+        title: "",
+        time: "Friday, February 23, 2024, 4:30 PM - 5:30 PM",
+        description: [
+            { line: "" },
+        ],
+        image: "/events/.jpg",
+    }, */
+    {
+        title: "SURF: Statistics Undergraduate Research Forum",
+        time: "Friday, February 28, 2024, 3:30 PM - 4:30 PM",
+        description: [
+            { line: "Join us for the Statistics Undergraduate Research Forum (SURF)! If you’re an undergraduate student interested in getting involved with statistics research but don’t know where to start, this is a great opportunity to hear from current seniors about their journeys in statistics research! Snacks will be provided😄 See you there! Please RSVP at tinyurl.com/gushsurf2025" },
+        ],
+        image: "/events/surf-2025.jpg",
+    },
+    {
+        title: "Spring Social",
+        time: "Friday, February 2025, 3:30 PM - 4:30 PM",
+        description: [
+            { line: "Come to the GUSH Spring Social this Friday, February 21 from 3:30-4:30pm in the Stat Lounge! Be sure to RSVP by 2/18 at tinyurl.com/gushspringsocial25 so we can get enough food for everyone :)" },
+        ],
+        image: "/events/spring-social-2025.jpg",
+    },
+    {
+        title: "MUSE: Mentorships for Undergraduates in Statistical Endeavors",
+        time: "Spring 2025",
+        description: [
+            { line: "Sign up for MUSE, GUSH’s spring undergraduate underclassmen-upperclassmen mentorship program! Fill out the form by Friday, February 21 to get matched and receive/give advice on stat courses, extracurriculars, internships, research, and more!" },
+        ],
+        image: "/events/muse-2025.jpg",
+    },
+    {
+        title: "Spring Course Panel",
+        time: "Monday, January 20, 2024, 8:00 PM - 9:00 PM",
+        description: [
+            { line: "GUSH will be hosting our annual Spring Statistics Courses panel on Monday, January 20 from 8-9pm ET on Zoom. We’ll have different breakout rooms for stats classes being offered this spring, where you’ll be able to talk to students who have taken the course or TFed the course in the past. This panel is meant for all students with any level of interest in statistics!" },
+        ],
+        image: "/events/spring-course-panel-2025.jpg",
+    },
+    {
+        title: "Intro to R Workshop",
+        time: "Thursday, January 16, 2025, 2:00 PM - 4:00 PM",
+        description: [
+            { line: "Join GUSH on January 16th from 2:00pm to 4:00pm Eastern Time for an introductory R workshop — no prior experience required!" },
+            { line: "The workshop will cover fundamental and widely-applicable skills such as using the R console, script editor, writing functions, looping, simulations, and plotting. This will equip you with the fundamental knowledge and a strong foundation to prepare for spring courses with an R component (such as Stat 111, Stat 104, Stat 102, Gov 50, Ec 20, Psych 1900, etc.) — or just learn the basics of R for fun! This workshop will be especially helpful for students taking Stat 111." },
+            { line: "Matt DiSorbo ‘17 will be teaching the workshop. Matt is a recent statistics alum who was an outstanding statistics concentrator and teaching fellow. Matt also has many years of experience using R in industry through his time at BlackRock, and he is currently a doctoral student at Harvard Business School." },
+            { line: "Be sure to RSVP, and we will see you there!" },
+        ],
+        image: "/events/intro-r-workshop-2025.jpg",
+    },
+    {
+        title: "Enhanced Stat Night: Thanksgiving",
+        time: "Tuesday, November 19, and Thursday, November 21, 2024, 7:00 PM - 9:00 PM",
+        description: [
+            { line: "Happy (early) Thanksgiving! This week will feature Enhanced Stat Night on Tuesday, 11/19 and Thursday, 11/21 (from 7-9 PM in Science Center 316). Share your gratitude and come celebrate with your favorite p-set buddies—with fall-themed snacks and baked goods!" },
+        ],
+        image: "/events/stat-night-thanksgiving-2024.jpg",
+    },
+    {
+        title: "HUGS: Help for Undergraduates from Graduate Students",
+        time: "Fall 2024",
+        description: [
+            { line: "Looking for a graduate student mentor to guide you through grad school apps? Or want to give advice to current undergrads? Sign up for HUGS (Help for Undergraduates from Graduate Students) to be paired with a mentor/mentee!" },
+        ],
+        image: "/events/hugs-2024.jpg",
+    },
+    {
+        title: "Stat Night: Grad School Edition",
+        time: "Thusday, October 10, 2024, 6:30 PM - 8:00 PM",
+        description: [
+            { line: "Are you thinking about applying to grad school or just interested in hearing current PhD candidates’ perspectives? GUSH and the Graduate Women in Statistics are hosting Stat Night: Grad School Edition on October 10! Everyone is welcome, and there will be food!" },
+        ],
+        image: "/events/stat-night-grad-school-2024.jpg",
+    },
+    {
+        title: "Fall Stats Social",
+        time: "Friday, September 20, 2024, 3:00 PM - 4:00 PM",
+        description: [
+            { line: "Join us for the GUSH Fall Social this Friday from 3-4pm! Location is TBD but will likely be in the Jefferson Labs Lot. Please RSVP and hope to see you there!" },
+        ],
+        image: "/events/fall-social-2024.jpg",
+    },
+    {
+        title: "Fall Course Panel",
+        time: "Wednesday, August 21, 2024, 7:00 PM - 8:00 PM",
+        description: [
+            { line: "Come attend GUSH’s annual Fall Course Panel! The virtual panel will be on August 21 from 7-8pm EDT. RSVP in the link above to learn about the Stat department’s fall course offerings from past students and TFs! All students welcome😁 Hope to see you there!" },
+        ],
+        image: "/events/fall-course-panel-2024.jpg",
+    },
+    {
+        title: "Summer Sibs",
+        time: "Summer 2024",
+        description: [
+            { line: "Calling all prefrosh and current statistics students! Sign up for GUSH’s Summer Sibs program to learn or give advice about academics, extracurriculars, research, and life at Harvard! Sign up by July 20th!" },
+        ],
+        image: "/events/summer-sibs-2024.jpg",
+    },
+    {
+        title: "Visi-Stats",
+        time: "Sunday, April 14, 2024, 12:00 PM - 1:00 PM",
+        description: [
+            { line: "Are you interested in studying statistics or a related field at Harvard? Join GUSH during our Visitas event to learn more about the department! We’ll be holding a short info session followed by some snacks! The RSVP link is in our bio." },
+        ],
+        image: "/events/visistats-2024.jpg",
+    },
+    {
+        title: "Spring Social",
+        time: "Friday, February 23, 2024, 4:30 PM - 5:30 PM",
+        description: [
+            { line: "Join us NEXT FRIDAY in SC 316 from 4:30-5:30 for a fun statistics social with catered pastries! Anyone who is affiliated with or interested in the statistics department is welcome to join :) Please RSVP at the form in our bio if you plan on attending so we can get a headcount for food!" },
+        ],
+        image: "/events/spring-social-2024.jpg",
+    },
+
+    //OLD 
+
     {
         title: "Stats on Stat Classes: 2023 Fall Courses Panel",
         time: "Wednesday, August 23, 2023, 7:00 PM - 8:00 PM",
