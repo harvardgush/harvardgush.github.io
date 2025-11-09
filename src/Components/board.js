0af6e015ca5f97e18213b78bb33f60a3bf8d3692
Eric Tong <erictong@college.harvard.edu>
2025-11-09 14:02:23 -0500

    Updated people

--- src/Components/board.js
+++ src/Components/board.js
@@ -24,22 +24,52 @@ exports.boardMembers = [
         desc: "Jesse is a sophomore in Leverett concentrating in statistics and mathematics. In addition to GUSH, Jesse is involved in HMMT, HUQT, and TFing for Stat 110. For fun, he likes to scuba dive and watch improvisational comedy."
     },
     {
-        name: "Eric Tong",
-        title: "Co-Treasurer",
-        imagePath: "/headshots/Eric Tong.jpg",
-        desc: "Eric is a sophomore in Leverett studying statistics. He enjoys solving puzzles, playing games of all sorts, juggling, and unicycling."
+        name: "Anthony Shen",
+        title: "Board Member",
+        imagePath: "/headshots/Anthony Shen.jpg",
+        desc: "Anthony is a Sophomore in Currier House concentrating in Statistics and Math. Outside of GUSH, he is involved with AADT and other research projects. In his free time, he likes playing basketball, card games, and trying new food places in the area!"
     },
     {
-        name: "Rebecca Xue",
-        title: "Co-Treasurer",
-        imagePath: "/headshots/Rebecca Xue.jpg",
-        desc: "Rebecca is a sophomore in Winthrop studying Molecular & Cellular Biology and Statistics. Apart from GUSH, Rebecca is involved in HUCBE, AADT, PBHA, and HRDH, and enjoys finding new cafes/restaurants and exploring Boston!"
+        name: "Brooke Liao",
+        title: "Board Member",
+        imagePath: "/headshots/Brooke Liao.jpg",
+        desc: "Brooke is a sophomore in Leverett house studying Statistics and Economics. Aside from GUSH, she's involved in CBE, VCG, HRO, and TFing STAT100. You can usually find her at any coffee spot in the square!"
+    },
+    {
+        name: "Cady Wang",
+        title: "Board Member",
+        imagePath: "/headshots/Cady Wang.jpeg",
+        desc: "Cady is a freshman studying Applied Mathematics and Statistics with a secondary in Computer Science. In her free time, she enjoys running, going to the gym, hiking, reading classics, and big backing around Boston."
+    },
+    {
+        name: "Emily Yu",
+        title: "Board Member",
+        imagePath: "/headshots/Emily Yu.jpg",
+        desc: "Hi all! I'm Emily, co '29 from Boston, but for this next year I'll be living in Lionel Hall. I'm looking to study MCB (or some sort of biology...) with Stats and a secondary in Social Studies. I spend most of my time outside class dancing with AADT, watching whatever YouTube Movies makes free, and playing piano. I love analyzing hot takes and playing mediocre table tennis, too. Super excited to meet the GUSH community!"
     },
     {
         name: "Elvin Lo",
         title: "Board Member",
         imagePath: "/headshots/Elvin Lo.jpg",
         desc: "Elvin is a junior in Dunster studying Statistics. Besides GUSH, he is involved with the Taiwanese Cultural Society and plays a lot of tennis. He also likes spikeball and is working on getting into mahjong."
+    },
+    {
+        name: "Eric Tong",
+        title: "Board Member",
+        imagePath: "/headshots/Eric Tong.jpg",
+        desc: "Eric is a sophomore in Leverett studying statistics. He enjoys solving puzzles, playing games of all sorts, juggling, and unicycling."
+    },
+    {
+        name: "Ethan Tan",
+        title: "Board Member",
+        imagePath: "/headshots/Ethan Tan.JPEG",
+        desc: "Ethan is a sophomore in Pforzheimer studying Statistics and Economics. In addition to GUSH, he's involved in HUCG, HSAC and plays club ultimate frisbee for Harvard. To spend his free time he enjoys watching sports, playing board games and practising card magic."
+    },
+    {
+        name: "Kerry Nguyen",
+        title: "Board Member",
+        imagePath: "/headshots/Joe Blitzstein.jpg",
+        desc: "Kerry is a senior in Quincy House studying Applied Mathematics and Physics. He is also involved with the Harvard Vietnamese Association and the Harvard Taekwondo Club. In his free time, he enjoys exploring new cafes in the Greater Boston area."
     },
     {
         name: "Lewis Tu",
@@ -52,6 +82,36 @@ exports.boardMembers = [
         title: "Board Member",
         imagePath: "/headshots/Melissa Wu.jpg",
         desc: "Melissa is a senior in Lowell studying Statistics and Computer Science with a secondary in Economics. Outside of GUSH, Melissa is a choreographer for AADT and a TF for the Stat Department. In her free time, she enjoys cafe-hopping and playing card games!"
+    },
+    {
+        name: "Rebecca Xue",
+        title: "Board Member",
+        imagePath: "/headshots/Rebecca Xue.jpg",
+        desc: "Rebecca is a sophomore in Winthrop studying Molecular & Cellular Biology and Statistics. Apart from GUSH, Rebecca is involved in HUCBE, AADT, PBHA, and HRDH, and enjoys finding new cafes/restaurants and exploring Boston!"
+    },
+    {
+        name: "Sirja Joeveer",
+        title: "Board Member",
+        imagePath: "/headshots/Joe Blitzstein.jpg",
+        desc: "Sirja is a freshman studying Mathematics and Statistics on the Machine Learning track. Along with GUSH, Sirja is a ballet dancer with the Harvard Ballet Company and violinist in the Mozart Society Orchestra. In her free time she loves to run, collect pink stationery, and watch Disney Channel original movies (DCOMs)."
+    },
+    {
+        name: "Sophia Liu",
+        title: "Board Member",
+        imagePath: "/headshots/Sophia Liu.png",
+        desc: "Sophia is a sophomore in Currier House studying Computer Science and Statistics. Outside of GUSH, she is involved in T4SG, HUQT, SWS, WECode, and research. In her free time, she can be found playing tennis or exploring new cafes and restaurants around Boston. "
+    },
+    {
+        name: "Sophia Liu",
+        title: "Board Member",
+        imagePath: "/headshots/Sophia Liu.jpeg",
+        desc: "Sophia is a freshman studying Math and Statistics. Outside of GUSH, she's involved with HackHarvard and biomedical research. In her free time, she enjoys running and trying new food!"
+    },
+    {
+        name: "Sophia Zhang",
+        title: "Board Member",
+        imagePath: "/headshots/Sophia Zhang.JPG",
+        desc: "Sophia is a sophomore studying Statistics and Comparative Literature hailing from California's Bay Area. In her free time, she loves reading, writing, and frolicking around in the outdoors."
     },
     {
         name: "Joe Blitzstein",
